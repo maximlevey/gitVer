9963a8aae65c60cd088fde0de230038a04ba4959
Maxim Levey <72744507+maximlevey@users.noreply.github.com>
2023-02-14 13:45:43 +1100

    Update gitVer_APT.sh

--- Scripts/gitVer_APT.sh
+++ Scripts/gitVer_APT.sh
@@ -5,7 +5,7 @@
 # gitVer_APT.sh
 # Checks if Git installed via apt-get and reports version to DataDog
 # Created by Maxim Levey <github.com/maximlevey>
-# Last Modified 12/02/2023
+# Last Modified 14/02/2023
 
 #---Requirements---#
 
